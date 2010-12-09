8f68c62ceb4fb020a1d90cf5588dd4a15eb67f36
Smurfmeister <simonmurphy66@hotmail.com>
2010-12-09 14:33:45 +0000

    Changed the merger method

--- kepler_processor/merger.rb
+++ kepler_processor/merger.rb
@@ -14,13 +14,17 @@ module KeplerProcessor
       #  end
 
       def merge!
-        # need to average the times over merge_ratio values, along with the corresponding fluxes, then replace by a single averaged value
-        # => sum #{merge_ratio} values
-        # => convert to float
-        # => divde by merge_ratio (to get average)
-        # => replace those #{merge_ratio} values by the single averaged value for each of time and flux
-        # => if t[ i + merge_ratio ] - t[i] > merge_ratio * standard_time_gap_between_points, do not replace this set of points
-        # => => this is so that when there is a time gap, we do not try to merge points over that time gap, just leave alone instead.
+        # the gap between consecutive points it time for slc data is just over 0.00068 and for llc data is just greater than 0.02
+        # therefore, if the data is slc, as indicated by input_filename, take the first value for std_range, if not, take second.
+        @input_filename.split("_")[1] = "slc" ? std_range = 0.00068 : std_range = 0.02
+        
+        # separate the input_data array into times and fluxes arrays, then slice those into arrays of size merge_ratio
+        # for each slice, replace the slice by the arithmetic mean value, unless there is a time gap in the data greater than the std gap
+        @times = @input_data.map { |column| column[0] }.each_slice(merge_ratio) do |slice|
+          slice = inject(:+).to_f / merge_ratio unless ( slice.last - slice.first ) > ( merge_ratio * std_range )
+        end
+        @fluxes = @input_data.map {|column| column [1] }.each_slice(merge_ratio) do |slice|
+          slice = inject(:+).to_f / merge_ratio unless ( slice.last - slice.first ) > ( merge_ratio * std_range )
       end
 
       def output_filename
