cb744ab90fa99a261be09d437c54a69c4b155872
Ben Langfeld <ben@langfeld.me>
2010-12-09 19:44:04 +0000

    Keep datapoints together

--- kepler_processor/merger.rb
+++ kepler_processor/merger.rb
@@ -16,14 +16,16 @@ module KeplerProcessor
 
         # separate the input_data array into times and fluxes arrays, then slice those into arrays of size merge_ratio
         # for each slice, replace the slice by the arithmetic mean value, unless there is a time gap in the data greater than the std gap
-        @times = []
-        @input_data.map { |column| column[0] }.each_slice(@options[:merge_ratio]) do |slice|
-          times.push inject(:+).to_f / @options[:merge_ratio] unless ( slice.last - slice.first ) > ( @options[:merge_ratio] * std_range )
+        @output_data = []
+        @input_data.each_slice(@options[:merge_ratio]) { |slice| @output_data.push slice }
+        @output_data.map! do |slice|
+          if ( slice.last[0] - slice.first[0] ) > ( @options[:merge_ratio] * std_range ) || ( slice.last[1] - slice.first[1] ) > ( @options[:merge_ratio] * std_range )
+            slice # Don't average things out
+          else
+            [slice.map { |e| e[0] }.inject(:+).to_f / @options[:merge_ratio], slice.map { |e| e[1] }.inject(:+).to_f / @options[:merge_ratio]]
+          end
         end
-        @fluxes = []
-        @input_data.map { |column| column[1] }.each_slice(@options[:merge_ratio]) do |slice|
-          fluxes.push inject(:+).to_f / @options[:merge_ratio] unless ( slice.last - slice.first ) > ( @options[:merge_ratio] * std_range )
-        end
+        @output_data.flatten!
       end
 
       def output_filename
