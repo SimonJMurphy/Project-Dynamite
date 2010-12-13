751cf51a816468ea43ec369b8a553c5c8828edb7
Smurfmeister <simonmurphy66@hotmail.com>
2010-12-13 22:41:13 +0000

    Correcting the flatten procedure. Needs testing.

--- kepler_processor/merger.rb
+++ kepler_processor/merger.rb
@@ -17,15 +17,15 @@ module KeplerProcessor
         # separate the input_data array into times and fluxes arrays, then slice those into arrays of size merge_ratio
         # for each slice, replace the slice by the arithmetic mean value, unless there is a time gap in the data greater than the std gap
         @output_data = []
-        @input_data.each_slice(@options[:merge_ratio]) { |slice| @output_data.push slice }
-        @output_data.map! do |slice|
+        @input_data.each_slice(@options[:merge_ratio]) do |slice|
           if ( slice.last[0] - slice.first[0] ) > ( @options[:merge_ratio] * std_range ) || ( slice.last[1] - slice.first[1] ) > ( @options[:merge_ratio] * std_range )
-            slice # Don't average things out
+            slice.each { |s| @output_data << s } # Don't average things out
           else
-            [slice.map { |e| e[0] }.inject(:+).to_f / @options[:merge_ratio], slice.map { |e| e[1] }.inject(:+).to_f / @options[:merge_ratio]]
+            @output_data << [slice.map { |e| e[0] }.inject(:+).to_f / @options[:merge_ratio], slice.map { |e| e[1] }.inject(:+).to_f / @options[:merge_ratio]]
           end
         end
-        @output_data.flatten!
+        puts @input_data[1420, 100].inspect
+        puts @output_data[142, 10].inspect
       end
 
       def output_filename
