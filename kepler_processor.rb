e2edad906e1df95324c2eee81f9d4ca2f018d584
Ben Langfeld <ben@langfeld.me>
2011-01-17 20:37:43 +0100

    Include IntervalArray class for turning a range into an array of values separated by a particular interval

--- kepler_processor.rb
+++ kepler_processor.rb
@@ -17,7 +17,7 @@ class Array
   def to_hash
     # create an empty hash with inject. The key is made lower case and spaces swapped to underscore.
     self.inject({}) do |accumulator, element|
-      accumulator[element[0].downcase.gsub(" ", "_").to_sym] = element[1].gsub(" ", "").strip 
+      accumulator[element[0].downcase.gsub(" ", "_").to_sym] = element[1].gsub(" ", "").strip
       accumulator
     end
   end
@@ -31,5 +31,41 @@ class Float
 
   def freq_to_per_day
     self / SECONDS_PER_DAY
+  end
+end
+
+class IntervalArray < Array
+  attr_reader :range, :step_size
+  def initialize(range, step_size = 1)
+    super()
+    @range = range
+    @step_size = step_size
+    regenerate
+  end
+
+  def range=(range)
+    @range = range
+    regenerate
+  end
+
+  def step_size=(step_size)
+    @step_size = step_size
+    regenerate
+  end
+
+  private
+    def regenerate
+      clear
+      self << @range.min
+      num_elements = (@range.count - 1) / @step_size
+      while count <= num_elements
+        self << last + @step_size
+      end
+    end
+end
+
+class Range
+  def in_steps_of(n = 1)
+    IntervalArray.new self, n
   end
 end
