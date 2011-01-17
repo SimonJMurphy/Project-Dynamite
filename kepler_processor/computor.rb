2b53e644533081fe729de49457fe9c3ca6957de1
Ben Langfeld <ben@langfeld.me>
2011-01-17 20:38:43 +0100

    Use IntervalArray to loop over frequencies we need

--- kepler_processor/computor.rb
+++ kepler_processor/computor.rb
@@ -8,20 +8,17 @@ module KeplerProcessor
     end
 
     def dft
-      final_frequency = 100.0
       dataset_length = @input_data.last[0] - @input_data.first[0]   # gives length of dataset in days by difference in final and initial time
       frequency_step = 1 / (10.0 * dataset_length)                  # step is 1/10T
+      frequencies = (0..100).in_steps_of frequency_step
 
       @output_data = {}
-      @input_data.each do |line|                        # |line| is representing 'i' - more intuitive and ruby-like
-        j = 0       # looping from zero is the same as setting the starting frequency of the range to be analysed to zero.
-        k = 0.0
+      @input_data.each do |line| # |line| is representing 'i' - more intuitive and ruby-like
         time = line[0]
         magnitude = line[1]
-        while k < final_frequency do |j|
-          k = j * frequency_step                        # k represents the frequency currently being looked at, or f_j
-          cos_i = Math.cos(2 * Math::PI * k * time)
-          sin_i = Math.sin(2 * Math::PI * k * time)
+        frequencies.each do |f|
+          cos_i = Math.cos(2 * Math::PI * f * time)
+          sin_i = Math.sin(2 * Math::PI * f * time)
 
           real_component += cos_i * magnitude           # the sum of all the cosine terms times the magnitudes
           imaginary_component += sin_i * magnitude
@@ -32,7 +29,6 @@ module KeplerProcessor
 
           # Output data is a hash of frequency-complex number pairs, with a new line for each frequency step.
           @output_data[k] += Complex(amp_j, phi_j)
-          j += 1
         end
       end
     end
