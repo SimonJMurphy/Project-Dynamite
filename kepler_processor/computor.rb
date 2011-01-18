cffac36754988969f96fa60e41c16515a6e42348
Ben Langfeld <ben@langfeld.me>
2011-01-18 14:36:36 +0100

    Make dft complete the fourier (get magnitudes of the correct scale) rather than the plot doing this

--- kepler_processor/computor.rb
+++ kepler_processor/computor.rb
@@ -15,10 +15,10 @@ module KeplerProcessor
 
       # Output data is a hash of frequency-complex number pairs, with a new line for each frequency step.
       @output_data = {}
-      
+
       # populates a hash with initial zero values (quicker here than in the loop)
       frequencies.each { |f| @output_data[f] = Complex(0,0) }
-      
+
       @input_data.each do |line|
         time, magnitude = line
         frequencies.each do |f|
@@ -26,6 +26,7 @@ module KeplerProcessor
           @output_data[f] += Complex(Math.cos(omega_t) * magnitude, Math.sin(omega_t) * magnitude) # complex(real, imaginary)
         end
       end
+      @output_data.each { |f, val| @output_data[f] = @output_data[f].magnitude * 2 / @input_data.size }
     end
 
     def plot
@@ -40,7 +41,7 @@ module KeplerProcessor
           plot.xlabel "Frequency (c/d)"
 
           x = @output_data.map { |pair| pair[0] }
-          y = @output_data.map { |pair| pair[1].magnitude * 2 / @input_data.size }
+          y = @output_data.map { |pair| pair[1] }
 
           plot.data << ::Gnuplot::DataSet.new( [x, y] ) do |ds|
             ds.with = "lines"
