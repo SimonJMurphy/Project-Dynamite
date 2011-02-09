666143b50d1e0778eb610276f527448c5f8253cb
Simon Murphy <simonmurphy66@hotmail.com>
2011-02-09 13:32:24 +0100

    tidy whitespace

--- ext/kepler_dft/kepler_dft.c
+++ ext/kepler_dft/kepler_dft.c
@@ -15,64 +15,64 @@ VALUE method_dft(VALUE self, VALUE times, VALUE magnitudes, VALUE number_of_poin
   const int num_points = NUM2INT(number_of_points);
   const double dataset_length = NUM2DBL(bandwidth), step_size = 1 / (20 * dataset_length), two_PI = 6.283185307179586477, start_frequency = 0, final_frequency = 24;
   double C_i, S_i, omega_t, magnitude, time, amplitude;
-	int i, j, k, num_frequencies = 0;
+  int i, j, k, num_frequencies = 0;
 
-	for(k = 0; start_frequency + (k * step_size) <= final_frequency; ++k)
-	{
-		++num_frequencies;
-	}
+  for(k = 0; start_frequency + (k * step_size) <= final_frequency; ++k)
+  {
+    ++num_frequencies;
+  }
 
-	double frequency_array[num_frequencies], real_component[num_frequencies], imaginary_component[num_frequencies];
+  double frequency_array[num_frequencies], real_component[num_frequencies], imaginary_component[num_frequencies];
 
-	for(k = 0; k < num_frequencies; ++k)
-	{
-		frequency_array[k] = start_frequency + (k * step_size);
-		real_component[k] = 0;
-		imaginary_component[k] = 0;
-	}
+  for(k = 0; k < num_frequencies; ++k)
+  {
+    frequency_array[k] = start_frequency + (k * step_size);
+    real_component[k] = 0;
+    imaginary_component[k] = 0;
+  }
 
-	// main loop
-	for (i = 0; i < num_points; ++i)
-	{
-		time = NUM2DBL(rb_ary_entry(times, i));
-		magnitude = NUM2DBL(rb_ary_entry(magnitudes, i));
+  // main loop
+  for (i = 0; i < num_points; ++i)
+  {
+    time = NUM2DBL(rb_ary_entry(times, i));
+    magnitude = NUM2DBL(rb_ary_entry(magnitudes, i));
 
-		// prepare SIN-Approximation
-		double ThisSin, LastSin, sin_wt, ThisCos, LastCos, cos_wt;
-		const double two_pi_t = two_PI * time;
+    // prepare SIN-Approximation
+    double ThisSin, LastSin, sin_wt, ThisCos, LastCos, cos_wt;
+    const double two_pi_t = two_PI * time;
 
-		double omega_0_t = two_pi_t * start_frequency;
-		double omega_step_t = two_pi_t * step_size;
+    double omega_0_t = two_pi_t * start_frequency;
+    double omega_step_t = two_pi_t * step_size;
 
-		LastSin = magnitude * sin(omega_0_t);
-		LastCos = magnitude * cos(omega_0_t);
-		sin_wt = sin(omega_step_t);
-		cos_wt = cos(omega_step_t);
+    LastSin = magnitude * sin(omega_0_t);
+    LastCos = magnitude * cos(omega_0_t);
+    sin_wt = sin(omega_step_t);
+    cos_wt = cos(omega_step_t);
 
-		double *real = real_component, *imaginary = imaginary_component;
-		for(j = 0; j < num_frequencies; ++j)
-		{
-			(*real++) += LastSin;
-			(*imaginary++) += LastCos;
+    double *real = real_component, *imaginary = imaginary_component;
+    for(j = 0; j < num_frequencies; ++j)
+    {
+      (*real++) += LastSin;
+      (*imaginary++) += LastCos;
 
-			ThisSin = LastSin * cos_wt + LastCos * sin_wt;
-			ThisCos = LastCos * cos_wt - LastSin * sin_wt;
-			LastSin = ThisSin;
-			LastCos = ThisCos;
-		}
-	} // end main loop
+      ThisSin = LastSin * cos_wt + LastCos * sin_wt;
+      ThisCos = LastCos * cos_wt - LastSin * sin_wt;
+      LastSin = ThisSin;
+      LastCos = ThisCos;
+    }
+  } // end main loop
 
-	double *real = real_component, *imaginary = imaginary_component;
-	for(j = 0; j < num_frequencies; ++j)
-	{
-		amplitude = 2 * sqrt((*real) * (*real) + (*imaginary) * (*imaginary)) / num_points;
-		rb_hash_aset(output, DBL2NUM(frequency_array[j]), DBL2NUM(amplitude));
-		real++; imaginary++;
-	}
+  double *real = real_component, *imaginary = imaginary_component;
+  for(j = 0; j < num_frequencies; ++j)
+  {
+    amplitude = 2 * sqrt((*real) * (*real) + (*imaginary) * (*imaginary)) / num_points;
+    rb_hash_aset(output, DBL2NUM(frequency_array[j]), DBL2NUM(amplitude));
+    real++; imaginary++;
+  }
 
-	// free(real_component);
-	// free(imaginary_component);
-	// free(frequency_array);
+  // free(real_component);
+  // free(imaginary_component);
+  // free(frequency_array);
 
-	return output;
+  return output;
 }
