d79dcfc7a471e572403e4cf0491fb83863b5f546
Simon Murphy <simonmurphy66@hotmail.com>
2011-02-14 14:15:42 +0000

    Extra idea for appender

--- lib/kepler_processor/tasks/appender.rb
+++ lib/kepler_processor/tasks/appender.rb
@@ -14,6 +14,7 @@ module KeplerProcessor
        # directly after last datapoint of first file, first datapoint of next file should be added to @input_data
        # continue for all files given
        # reinsert the partitioned comments, but change the 'season' parameter's value to read 'multiple' or something more specific if possible
+       # could ask for user input for 'season' parameter
        # require condition that there are at least two files.
        # all files must have the same kic number
      end
