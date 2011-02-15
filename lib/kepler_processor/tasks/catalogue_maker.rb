52015da3787097c7f1b882aa808b92c41e3e5463
Simon Murphy <simonmurphy66@hotmail.com>
2011-02-15 12:35:49 +0000

    add hashes for observations for catalogue

--- lib/kepler_processor/tasks/catalogue_maker.rb
+++ lib/kepler_processor/tasks/catalogue_maker.rb
@@ -8,32 +8,41 @@ module KeplerProcessor
     end
 
     class Run < TaskRunBase
+      CATALOGUE_IMAGES_PATH = "Users/sjm/code/Project-Dynamite/data/output/wg4_catalogue_images/"
       def run
         super do
-          create_observation_index_hash
-          build_up_data
+          create_star_metadata_hash
+          create_observation_index
           create_pdf
         end
       end
 
-      def create_observation_index_hash
-        puts @input_data.inspect
-        @star_metadata = { 10000056 => { :log_g => 5832958, :feh => 427482, :t_eff => 4387238, :radius => 2321 } }
-        # use this to get kic nums and corresponding parameters
-        # one key:value pair for kic num => other values, then one for each of those values representing parameter => corresponding numerical value
+      def create_star_metadata_hash
+        @star_metadata = {}
+        @input_data.each do |star|
+          @star_metadata[star[0].to_i] = { :magnitude => star[3], :t_eff => star[4], :radius => star[5], :log_g => star[6], :feh => star[7], :contamination => star[8] }
+        end
+      end
+
+      def create_observation_index
+        @observation_index = @input_data.map do |observation|
+          kic_number, cadence, season = observation
+          kic_number = kic_number.to_i
+          cadence = cadence == "SC" ? "slc" : "llc"
+          hash = { :kic_number => kic_number, :cadence => cadence, :season => season, :cycle => "kic#{kic_number} #{season} #{cadence}", :lightcurve_path => "#{CATALOGUE_IMAGES_PATH}kic#{kic_number}_CFlux_#{season}_#{cadence}_plot.png", :short_fourier_path => "#{CATALOGUE_IMAGES_PATH}kic#{kic_number}_CFlux_#{season}_#{cadence}_fourier_plot_0to24.png" }
+          hash[:long_fourier_path] = "#{CATALOGUE_IMAGES_PATH}kic#{kic_number}_CFlux_#{season}_#{cadence}_fourier_plot_0to100.png" if cadence == "slc"
+          hash
+        end
       end
 
       # input_filenames of the form:        kic10000056_CFlux_Q4.2_slc.txt
       # lightcurve_filenames of the form:   kic10000056_CFlux_Q4.2_slc_plot.png
       # fourier_plot_filenames of the form: kic10000056_CFlux_Q4.2_slc_fourier_plot_0to100.png
 
-      def build_up_data
-        @page_data = [{ :kic_number => 10000056, :cycle => "kic10000056 Q4.2 slc", :lightcurve_path => "kic10000056_CFlux_Q4.2_slc_plot.png", :long_fourier_path => "kic10000056_CFlux_Q4.2_slc_fourier_plot_0to100.png", :short_fourier_path => "kic10000056_CFlux_Q4.2_slc_fourier_plot_0to24.png" }]
-      end
-
       def create_pdf
+        observation_index = @observation_index
         Prawn::Document.generate(full_output_filename, :page_layout => :portrait, :margin => 5, :skip_page_creation => false) do
-          @page_data.each do |observation|
+          observation_index.each do |observation|
             start_new_page
 
             image observation[:lightcurve_path], :at => [0,750], :width => 580
@@ -46,7 +55,7 @@ module KeplerProcessor
 
             draw_text "log g \t = #{star_metadata[:log_g]}", :at => [420, 770]
             draw_text "[Fe/H] = #{star_metadata[:feh]}", :at => [420, 755]
-            draw_text "t_eff \t = #{star_metadata[:t_eff]}", :at => [520, 770]
+            draw_text "Teff \t = #{star_metadata[:t_eff]}", :at => [520, 770]
             draw_text "radius = #{star_metadata[:radius]}", :at => [520, 755]
           end
 
