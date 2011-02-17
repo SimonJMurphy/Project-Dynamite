0644c6ceb511ae0745abb8c89bd0ddb9a8c61552
Simon Murphy <simonmurphy66@hotmail.com>
2011-02-17 15:20:34 +0000

    correct slicer's interpretation of slice_size from user input

--- lib/kepler_processor/tasks/slicer.rb
+++ lib/kepler_processor/tasks/slicer.rb
@@ -16,12 +16,13 @@ module KeplerProcessor
 
         def slice
           # identify expected space between points, which may or may not be useful
-          std_range = @input_filename.split("_")[3] == "slc" ? 0.00068 : 0.02
+          std_range = @input_filename.split("_").last.split(".").first == "slc" ? 0.00068 : 0.02
+          @slice_size = @options[:slice_size] / std_range
 
           # slice the input data array into arrays of size slice_size
           # for each slice, save a series of output files named according to position in time (eg. @input_filename_slice4).
           @slices = []
-          @input_data.each_slice(@options[:slice_size]) { |slice| @slices << slice }
+          @input_data.each_slice(@slice_size) { |slice| @slices << slice }
         end
 
         def output_filename
