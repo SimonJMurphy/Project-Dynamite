da8e31e95e4e3275cd0bb9383c361210ffc434a1
Ben Langfeld <ben@langfeld.me>
2011-02-22 12:27:08 +0000

    Add test coverage of monkey patches

--- spec/kepler_processor/monkey_patches_spec.rb
+++ spec/kepler_processor/monkey_patches_spec.rb
@@ -1,9 +1,33 @@
 require 'spec_helper'
 
 describe Array do
-  pending "write it"
+  describe "to hash" do
+    it "should map to correct keys and values" do
+      [["foo", "bar"], ["something", "nothing"]].to_hash.should == { :foo => "bar", :something => "nothing" }
+    end
+
+    it "should make keys lower case" do
+      [["FOO", "bar"]].to_hash.should == { :foo => "bar" }
+    end
+
+    it "should replace spaces in keys with underscores" do
+      [["FOO FOO", "bar"]].to_hash.should == { :foo_foo => "bar" }
+    end
+  end
 end
 
 describe Float do
-  pending "write it"
+  describe "should round to" do
+    it "a float" do
+      Math::PI.round_to.should be_instance_of(Float)
+    end
+
+    it "zero decimal places by default" do
+      Math::PI.round_to.should == 3.0
+    end
+
+    it "a specified number of decimal places" do
+      Math::PI.round_to(2).should == 3.14
+    end
+  end
 end
