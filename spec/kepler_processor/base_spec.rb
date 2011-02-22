3a852975b27add7a450ed6c0b84ea64e53912dee
Ben Langfeld <ben@langfeld.me>
2011-02-22 13:47:26 +0000

    Add some basic and pending specs for Base and TaskRunBase

--- spec/kepler_processor/base_spec.rb
+++ spec/kepler_processor/base_spec.rb
@@ -1,5 +1,26 @@
 require 'spec_helper'
 
 describe KeplerProcessor::Base do
-  pending "write it"
+  it "should assign options to an instance variable" do
+    options = { :foo => :bar }
+    KeplerProcessor::Base.new(options).instance_variable_get(:"@options").should == options
+  end
+
+  describe "when running" do
+    it "should take a runner class as an argument to run" do
+      pending
+    end
+
+    it "should run for each input filepath provided" do
+      pending
+    end
+
+    it "should rescue FileExistsError and log an appropriate message" do
+      pending
+    end
+
+    it "should rescue other exceptions and log the exception message" do
+      pending
+    end
+  end
 end
--- spec/kepler_processor/task_run_base_spec.rb
+++ spec/kepler_processor/task_run_base_spec.rb
@@ -1,5 +1,96 @@
 require 'spec_helper'
 
 describe KeplerProcessor::TaskRunBase do
-  pending "write it"
+  before(:each) do
+    @options = { :foo => :bar, :output_path => "output_path" }
+    @complete_filename = "/Users/simon/filename.txt"
+    @trb = KeplerProcessor::TaskRunBase.new(@complete_filename, @options)
+  end
+
+  describe "on instantiation" do
+    it "should assign options to an instance variable" do
+      @trb.instance_variable_get(:"@options").should == @options
+    end
+
+    it "should assign filename to an instance variable" do
+      @trb.instance_variable_get(:"@input_filename").should == @complete_filename
+    end
+
+    it "should assign filename to an instance variable" do
+      @trb.instance_variable_get(:"@input_filename_without_extension").should == "filename"
+    end
+
+    it "should assign an empty array for input data to an instance variable" do
+      @trb.instance_variable_get(:"@input_data").should == []
+    end
+  end
+
+  it "should determine correct full output filename" do
+    @trb.should_receive(:output_filename).and_return("output_file.txt")
+    @trb.full_output_filename.should == "output_path/output_file.txt"
+  end
+
+  it "should have a nil output filename by default" do
+    @trb.send(:output_filename).should be_nil
+  end
+
+  describe "reading in data" do
+    it "should read data from the input filename" do
+      pending
+    end
+
+    it "should split columns by the appropriate delimiter" do
+      pending
+    end
+
+    it "should convert columns by the appropriate converters" do
+      pending
+    end
+
+    it "should raise NoDataError if the input file is empty" do
+      pending
+    end
+  end
+
+  it "should split comments (lines starting with a hash) from input data into an instance variable" do
+    pending
+  end
+
+  it "should get attributes from comment header" do
+    pending
+  end
+
+  it "should split out the appropriate columns" do
+    pending
+  end
+
+  describe "saving output file" do
+    it "should not do anything if output filename is nil" do
+      pending
+    end
+
+    it "should save contents of output data instance variable to correct output file" do
+      pending
+    end
+
+    it "should save from input data instance variable if no output data has been set" do
+      pending
+    end
+
+    describe "when force overwriting" do
+      it "should not raise FileExistsError" do
+        pending
+      end
+    end
+
+    describe "when not force overwriting" do
+      it "should raise FileExistsError if a file exists at the output path" do
+        pending
+      end
+    end
+
+    it "should log with appropriate output file name at the info level" do
+      pending
+    end
+  end
 end
