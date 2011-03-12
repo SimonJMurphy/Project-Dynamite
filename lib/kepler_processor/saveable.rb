4c39ba176b887aa20e76cd6497c72349d3cf015a
Ben Langfeld <ben@langfeld.me>
2011-03-12 17:19:11 +0000

    Make Saveable a little prettier

--- lib/kepler_processor/saveable.rb
+++ lib/kepler_processor/saveable.rb
@@ -1,5 +1,6 @@
 module KeplerProcessor
   module Saveable
+
     def full_output_filename
       "#{@options[:output_path]}/#{output_filename}"
     end
@@ -9,16 +10,17 @@ module KeplerProcessor
     end
 
     def save!
-      if output_filename
-        od = output_data || input_data || []
-        ::FileUtils.mkpath options[:output_path]
-        raise FileExistsError if File.exist?(full_output_filename) && !options[:force_overwrite]
-        LOGGER.info "Writing output to #{full_output_filename}"
-        CSV.open(full_output_filename, options[:force_overwrite] ? "w+" : "a+", :col_sep => "\t") do |csv|
-          # impicitly truncate file by file mode when force overwriting
-          od.each { |record| csv << record }
-        end
+      return unless output_filename
+      od = output_data || input_data || []
+      ::FileUtils.mkpath options[:output_path]
+      raise FileExistsError if File.exist?(full_output_filename) && !options[:force_overwrite]
+      LOGGER.info "Writing output to #{full_output_filename}"
+      CSV.open(full_output_filename, options[:force_overwrite] ? "w+" : "a+", :col_sep => "\t") do |csv|
+        # impicitly truncate file by file mode when force overwriting
+        od.each { |record| csv << record }
       end
+      true
     end
+
   end
 end
