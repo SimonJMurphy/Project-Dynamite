6894f64064bb1e5004c08a5cbf85cada351ceb48
Simon Murphy <simonmurphy66@hotmail.com>
2011-06-28 21:59:27 +0100

    Generalise comments for merger

--- lib/kepler_processor/tasks/merger.rb
+++ lib/kepler_processor/tasks/merger.rb
@@ -15,12 +15,12 @@ module KeplerProcessor
       private
 
         def merge!
-          # the gap between consecutive points it time for slc data is just over 0.00068 and for llc data is just greater than 0.02
-          # therefore, if the data is slc, as indicated by input_filename, take the first value for std_range, if not, take second.
+          # the gap in time between consecutive points for SC data is just over 0.00068 and for LC data is just greater than 0.02
+          # therefore, if the data is SC, as indicated by input_filename, take the first value for std_range, if not, take second.
           std_range = @input_filename.split("_")[3] == "slc" ? 0.00068 : 0.02
 
           # slice the input data array into arrays of size merge_ratio
-          # for each slice, replace the slice by the arithmetic mean value, unless there is a time gap in the data greater than the std gap or <10 items in slice (eof).
+          # for each slice, replace the slice by the arithmetic mean value, unless there is a time gap in the data greater than the std gap or <[merge_ratio] items in slice (eof).
           @output_data = []
           @input_data.each_slice(@options[:merge_ratio]) do |slice|
             if ( slice.last[0] - slice.first[0] ) > ( @options[:merge_ratio] * std_range ) || slice.size < @options[:merge_ratio]
