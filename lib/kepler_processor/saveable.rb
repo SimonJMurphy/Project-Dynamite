93e14332f17f7f67c791b8140001bd149dd60f26
Simon Murphy <simonmurphy66@hotmail.com>
2012-04-25 21:45:05 +0100

    Allow appending to a file.

--- lib/kepler_processor/saveable.rb
+++ lib/kepler_processor/saveable.rb
@@ -9,13 +9,13 @@ module KeplerProcessor
       nil # defaults to nil, child class must override output_filename in order to save
     end
 
-    def save!
+    def save!(append = false)
       return unless output_filename
       od = output_data || input_data || []
       ::FileUtils.mkpath options[:output_path]
       raise FileExistsError if File.exist?(full_output_filename) && !options[:force_overwrite]
-      LOGGER.info "Writing output to #{full_output_filename}"
-      CSV.open(full_output_filename, options[:force_overwrite] ? "w+" : "a+", :col_sep => "\t") do |csv|
+      LOGGER.info "Writing output to #{full_output_filename}: #{od.inspect}"
+      CSV.open(full_output_filename, append ? "a" : "w+", :col_sep => "\t") do |csv|
         # impicitly truncate file by file mode when force overwriting
         od.each { |record| csv << record }
       end
