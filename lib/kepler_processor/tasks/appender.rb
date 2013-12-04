b8fa8e1dde716c2950dda73874b3fb071841533c
Simon Murphy <simonmurphy66@hotmail.com>
2013-12-04 14:26:00 +1100

    Change appender to work with converted data. Allow multiple different stars.

--- lib/kepler_processor/tasks/appender.rb
+++ lib/kepler_processor/tasks/appender.rb
@@ -6,40 +6,45 @@ module KeplerProcessor
 
     def execute!
       super do
-        check_consistent_kic_number
-        sort_runners_by_season
-        collate_input_data
-        reinsert_header
-        save!
+        get_kic_numbers
+        separate_by_kic_number
+        process_stars
       end
     end
 
     private
 
-      def check_consistent_kic_number
-        raise(RuntimeError, "All files must be for the same star") if @runners.map { |r| r.attributes[:kic_number] }.uniq.count > 1
+      def get_kic_numbers
+        @kic_numbers = @runners.map { |r| r.attributes[:kic_number] }.uniq
+        # raise(RuntimeError, "All files must be for the same star") if @runners.map { |r| r.attributes[:kic_number] }.uniq.count > 1
       end
 
-      def sort_runners_by_season
-        @runners.sort! { |a,b| a.attributes[:season] <=> b.attributes[:season] }
+      def separate_by_kic_number
+        @stars = []
+        @kic_numbers.each do |kic|
+          this_star = Array.new
+          @runners.each do |runner|
+            this_star << runner if kic == runner.attributes[:kic_number]
+          end
+          @stars << this_star
+        end
+      end
+
+      def process_stars
+        @stars.each_with_index do |star, index|
+          @index = index
+          collate_input_data
+          save!
+        end
       end
 
       def collate_input_data
-        @output_data = @runners.map { |runner| runner.input_data }.flatten 1
-      end
-
-      def reinsert_header
-        @output_data.insert 0, ["# KIC number: #{@runners.first.attributes[:kic_number]}"]
-        @output_data.insert 0, ["# Season: #{season_range}"]
-      end
-
-      def season_range
-        @season_range ||= "#{@runners.first.attributes[:season]}-#{@runners.last.attributes[:season]}"
+        @output_data = @stars[@index].map { |runner| runner.input_data }.flatten 1
       end
 
       def output_filename
-        flux_type = @options[:file_columns] == [0, 1] ? :SAP : :msMAP
-        @runners.first.input_filename_without_path.sub(/\d{13}/, "#{flux_type}_appended_#{season_range}") # Timestamp always has 13 digits
+        flux_type = @stars[@index].first.input_filename_without_path.split("_")[1]
+        "#{@stars[@index].first.attributes[:kic_number]}_appended-#{flux_type}_Q99_llc.txt"
       end
   end
 end
